791b591245fe7ec10377117f1c7b761855480930
vitoria Farias <vitoriafarias1604@gmail.com>
2026-02-24 22:13:45 -0300

    Aula 1 - Variáveis em JavaScript

---
+++ Aula1/src/script.js
@@ -0,0 +1,12 @@
+console.log('Testando JavaScript');
+
+//Exemplos de variável let
+let nome = 'Vitoria';
+let nome2 = 'Farias'
+
+console.log(nome, nome2);
+
+//Exemplos de variável const
+const times = ['Corinthians', 'São Paulo', 'Santos'];
+
+console.log(times);
