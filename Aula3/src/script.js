8bf333c7d98e41f3c261b1ba1ea0a395dfb53bc0
vitoria Farias <vitoriafarias1604@gmail.com>
2026-02-26 22:21:32 -0300

    Aula 3 - Lógica de Programação(Introdução)

--- Aula3/src/script.js
+++ Aula3/src/script.js
@@ -1,2 +1,46 @@
-// Exemplo de operação aritmética
-let a = 5;
+//Exemplo de Condição Lógica: If e Else
+let x = 8;
+
+if (x == 5) {
+    //console.log('A variável x é igual a 5!');
+} else {
+    //console.log('A variável x não é igual a 5!');
+}
+
+//Exemplo de Condição Lógica: If Else If
+let y = 2;
+
+if (y > 3) {
+    console.log('A variável y é maior que 3!');
+} else if (y == 3) {
+    console.log('A variável y é igual a 3');
+} else {
+    //console.log('A variável y possui um valor diferente das verificações.');
+}
+
+//Exemplo e if
+const elemento = document.getElementById('ex1');
+//console.log(elemento);
+const pegarHoras = new Date().getHours();
+const pegarMinutos = new Date().getMinutes();
+const hora = String(pegarHoras) + ":" + String(pegarMinutos);
+
+//console.log(hora);
+
+if (pegarHoras > 18) {
+    elemento.innerHTML = "Boa Noite!";
+    elemento.style.color = "Blue";
+}
+
+//Exemplo if 2
+let idade = 18;
+let texto = 'Você é novo demais para dirigir!';
+
+if (idade >= 18) {
+    texto = 'Você tem idade para dirigir!';
+    
+}
+document.getElementById('idade').innerHTML = 'Sua idade é: ' + idade;
+
+document.getElementById('ex2').innerHTML = texto;
+
