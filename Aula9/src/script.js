56f8fc2e54f08750a684d3220e16f80611ffd18c
vitoria Farias <vitoriafarias1604@gmail.com>
2026-03-09 19:56:39 -0300

    Aula 9 - Métodos de Número em JavaSript

---
+++ Aula9/src/script.js
@@ -0,0 +1,34 @@
+// Utilização do método toString()
+let numero = 123;
+let exToString = numero.toString();
+console.log(typeof exToString);
+
+
+// Utilização do método toExponential()
+let a = 9.646;
+let exToExponential = a.toExponential(2);
+console.log(exToExponential);
+
+
+// Utilização do método toFixed()
+let b = 16.2761321;
+let exToFixed = b.toFixed(3);
+console.log(exToFixed);
+
+
+// Utilização do método toPrecision()
+let c = 21.13251627;
+let exToPrecision = c.toPrecision(4);
+console.log(exToPrecision);
+
+
+// Utilização do método valueOf()
+let d = 1977;
+let exValueOf = d.valueOf();
+console.log(exValueOf);
+
+
+// Utilização do método Number()
+let e = "166";
+let exNumber = Number(e);
+console.log(exNumber);
