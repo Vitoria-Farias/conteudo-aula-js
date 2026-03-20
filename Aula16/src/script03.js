7ede445831e50f51e45c8f9e94d37d1ce8bd17f7
vitoria Farias <vitoriafarias1604@gmail.com>
2026-03-19 21:27:16 -0300

    Aula 16 - Estudando validações em formulário

---
+++ Aula16/src/script03.js
@@ -0,0 +1,10 @@
+// Validação de formulário
+
+function validarForm() {
+    const formulario = document.forms["novoForm"]["texto"].value;
+    if (formulario == "") {
+        alert("Por favor, insira um valor para o campo!");
+        return false;
+    }
+}
+
