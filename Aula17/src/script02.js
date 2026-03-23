27ee2aff8a224c976cc1368946660597314d7f4c
vitoria Farias <vitoriafarias1604@gmail.com>
2026-03-23 18:56:22 -0300

    Aula 17 - Mouse Events

---
+++ Aula17/src/script02.js
@@ -0,0 +1,26 @@
+// Utilizando o evento mouseover
+const caixa = document.querySelector(".caixa");
+
+caixa.addEventListener("mouseover", function () {
+    caixa.innerHTML = "O mouse está dentro da caixa.";
+    caixa.style.background = "gray";
+    caixa.style.color = "black";
+});
+
+caixa.addEventListener("mouseout", function() {
+    caixa.innerHTML = "O mouse está fora da caixa.";
+    caixa.style.background = "rgb(1, 1, 31)";
+    caixa.style.color = "white";
+});
+
+
+// Exemplo de posição do mouse na página
+const conteudo = document.querySelector(".conteudo");
+
+document.addEventListener("mousemove", handlePosicao);
+
+function handlePosicao(evento) {
+    conteudo.innerHTML = "X: " + evento.clientX + " " + "Y: " + evento.clientY;
+    
+};
+
