76e984d729922dd806fcaa40d7136134aa0f0a98
vitoria Farias <vitoriafarias1604@gmail.com>
2026-03-27 19:59:35 -0300

    Aula 20 - Finalização de Eventos

---
+++ Aula20/src/script.js
@@ -0,0 +1,22 @@
+// Estudando preparação de eventos
+const div1 = document.querySelector(".div1");
+const p1 = document.querySelector(".p1");
+const div2 = document.querySelector(".div2");
+const p2 = document.querySelector(".p2");
+
+p1.addEventListener("click", () => {
+    alert("Você clicou no elemento branco.")
+}, false);
+
+div1.addEventListener("click", () => {
+    alert("Você clicou no elemento cinza.")
+}, false);
+
+p2.addEventListener("click", () => {
+    alert("Você clicou no elemento branco.")
+}, true);
+
+div2.addEventListener("click", () => {
+    alert("Você clicou no elemento cinza.")
+}, true);
+
